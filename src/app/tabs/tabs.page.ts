e139e09d61f4558d4d1584ec89be489096748e44
Tsvetelin Yakimov <ceci2205@abv.bg>
2025-11-30 01:32:11 +0200

    Add more tabs (placeholders for now)

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,13 +1,23 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import {
-  IonTabs,
-  IonTabBar,
-  IonTabButton,
   IonIcon,
   IonLabel,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import {
+  barChartOutline,
+  ellipse,
+  person,
+  personOutline,
+  square,
+  swapHorizontalOutline,
+  timerOutline,
+  triangle,
+  walletOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -19,6 +29,16 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({
+      triangle,
+      ellipse,
+      square,
+      personOutline,
+      person,
+      barChartOutline,
+      swapHorizontalOutline,
+      walletOutline,
+      timerOutline,
+    });
   }
 }
