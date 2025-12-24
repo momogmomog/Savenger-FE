ae867e8951ce77df28752e3089179783558ea9d6
Tsvetelin Yakimov <ceci2205@abv.bg>
2025-12-24 18:15:00 +0200

    Extend the Formified form wrapper with support for complex structures.

--- src/app/shared/util/forms.util.ts
+++ src/app/shared/util/forms.util.ts
@@ -1,4 +1,4 @@
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 /**
  * This utility converts every property of T into a FormControl of that property
@@ -24,6 +24,42 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
  */
 export type Formified<T> = FormGroup<{
   [K in keyof T]: FormControl<T[K]>;
+}>;
+
+/**
+ * More complex solution to Formified in case the payload contains more complex types such as arrays.
+ *
+ * Example with form payload that contains array:
+ * export interface ExamplePayload {
+ *   values: string[];
+ *   customs: CustomVal[];
+ * }
+ *
+ * export interface CustomVal {
+ *   name: string;
+ * }
+ *
+ * How the form construction would look like:
+ *  form!: DeepFormified<ExamplePayload>;
+ *
+ *  this.form = new FormGroup({
+ *      values: new FormArray<FormControl<string>>([]),
+ *      customs: new FormArray<Formified<CustomVal>>([]),
+ *  });
+ *
+ */
+export type DeepFormified<T> = FormGroup<{
+  [K in keyof T]: T[K] extends Array<infer U> // 1. Is it an Array?
+    ? FormArray<
+        // If it's an array of objects, wrap them in FormGroup.
+        // If it's an array of primitives (strings), wrap in FormControl.
+        U extends object ? DeepFormified<U> : FormControl<U>
+      >
+    : T[K] extends Date // 2. Is it a Date? (Dates are objects, catch them first!)
+      ? FormControl<T[K]>
+      : T[K] extends object // 3. Is it a nested Object?
+        ? DeepFormified<T[K]> // Recursion: Go deeper
+        : FormControl<T[K]>; // 4. Default: Primitives (string, number, boolean)
 }>;
 
 export class FormUtil {
